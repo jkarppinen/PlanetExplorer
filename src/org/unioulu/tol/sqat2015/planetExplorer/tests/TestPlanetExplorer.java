39255c2ef11dda451898fd5837e638a6ff7c74f1
somename <someemail>
2015-10-21 14:50:05 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,22 +3,15 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.unioulu.tol.sqat2015.planetExplorer.Planet;
 import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
-	@Test
-	public void testPlanetSize() {
-		Planet p = new Planet(100,100);
-		assertEquals(100,p.getSizeX());
-		assertEquals(100,p.getSizeY());
-	}
+
 	
 	@Test
-	public void landExplorer(){
-		Planet p = new Planet(100,100);
-		PlanetExplorer pe = new PlanetExplorer();
+	public void initPlanetExplorer(){
+		PlanetExplorer pe = new PlanetExplorer(100,100,"");
 		
 	}
 }
