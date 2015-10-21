90bfe6aca87bb36894f3f5e56c5564ccdf5a4bad
somename <someemail>
2015-10-21 14:45:57 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
@@ -1,8 +1,10 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
 public class Planet {
-
+	private int sizeX;
+	private int sizeY;
 	public Planet(int x, int y){
-		
+		this.sizeX = x;
+		this.sizeY = y;
 	}
 }
--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,6 +3,7 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.Planet;
 
 public class TestPlanetExplorer {
 
