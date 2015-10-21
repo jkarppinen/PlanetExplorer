9300b54a7145a26c1fe2459860e3ba6bc927fd49
somename <someemail>
2015-10-21 14:52:45 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -13,5 +13,10 @@ public class TestPlanetExplorer {
 	public void initPlanetExplorerLocationX(){
 		PlanetExplorer pe = new PlanetExplorer(100,100,"");
 		assertEquals(0,pe.getLocationX());
+	}	
+	@Test
+	public void initPlanetExplorerLocationY(){
+		PlanetExplorer pe = new PlanetExplorer(100,100,"");
+		assertEquals(0,pe.getLocationY());
 	}
 }
