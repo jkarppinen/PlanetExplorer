a16c809ce52c397e30bc6d38808fbe41019a3c1d
somename <someemail>
2015-10-21 15:40:18 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -92,7 +92,7 @@ public class PlanetExplorer {
 			
 				
 			if(command_arr[i] == "r"){
-				int new_position = ( this.getPosition_id() + 1 );
+				int new_position = this.getPosition_id() + 1;
 				this.setPosition_id(new_position);
 				this.setPosition(positions.get(this.getPosition_id()));
 			}
